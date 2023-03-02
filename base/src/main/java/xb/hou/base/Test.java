6d6e92cc774a28129955f03295b2072eec00bba1
xbhou-cn <819188546@qq.com>
2023-03-02 15:53:01 +0800

    Initial commit

---
+++ base/src/main/java/xb/hou/base/Test.java
@@ -0,0 +1,11 @@
+package xb.hou.base;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        double s = a;
+        float f = a;
+        int b = a + 1 ;
+        System.out.println(b);
+    }
+}
